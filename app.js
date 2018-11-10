b3edf23611bd18084c6dfbf231e66a337f12f54b
abdelaziz <abdelaziz.iddouch@gmail.com>
2018-11-10 23:41:09 +0000

    initial commit , just set up express and handlebars

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+// handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// index route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// about
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port} 🚀🚀🚀🚀`);
+});
